d42c4ae7ec8c8741fb6cf1d0f74508306f5c5c21
Abhishek Shaw <abhishekshaw21@gmail.com>
2017-03-12 14:12:13 +0530

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -18,6 +18,13 @@ class Teacher
 		for(int i=4;i<7;i++)
 			timetable[5][i].subjectID="X";
 	}
+	public evaluate_heuristic()
+	{
+		int first_period_weight=1000;
+		int non_teaching_weight=500;
+		int day_ratio_weight=100;
+		
+	}
 	public void notavailable(int day,int start_period,int end_period)
 	{
 			for(int i=start_period;i<=end_period;i++)
@@ -45,7 +52,7 @@ class Teacher
 		int non_teaching=non_teaching_day();
 		int first_period;
 	}
-	public int non_teachingday(){
+	private boolean non_teachingday(){
 		int day=0;
 		for(int i=0;i<6;i++)
 		{
@@ -55,11 +62,11 @@ class Teacher
 						break;
 			}
 			if(j==7)
-				day++;
+				return false;
 		}
-		return day;
+		return true;
 	}
-	public int first_period(){
+	private static int first_period(){
 		int day=0;
 		for(int i=0;i<6;i++)
 		{
@@ -68,7 +75,7 @@ class Teacher
 		}
 		return day;	
 	}
-	public float day_ratio(int day)
+	static private float day_ratio(int day)
 	{
 		int unassigned=0,assigned=0;
 		for(int j=0;j<7;j++)
